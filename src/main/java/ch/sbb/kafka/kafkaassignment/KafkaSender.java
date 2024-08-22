516fe0747332cd3089f1da907ad5024e41b3646e
Adrian Gygax <adrian.gygax2@sbb.ch>
2024-08-22 15:53:31 +0200

    add as much config as possible

--- src/main/java/ch/sbb/kafka/kafkaassignment/KafkaSender.java
+++ src/main/java/ch/sbb/kafka/kafkaassignment/KafkaSender.java
@@ -1,5 +1,6 @@
 package ch.sbb.kafka.kafkaassignment;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -16,7 +17,10 @@ public class KafkaSender {
     private Properties createConnectionProperties() {
         Properties props = new Properties();
 
-        // Insert connection properties here. You'll get them in the meeting.
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "pkc-zpjg0.eu-central-1.aws.confluent.cloud:9092");
+        props.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"REPLACEME\" password=\"REPLACEME\";");
+        props.put("sasl.mechanism", "PLAIN");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
 
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
